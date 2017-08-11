1c20a70b872d652b8075b7af4285e93e490bf185
YannThorimbert <yann.thorimbert@gmail.com>
2017-08-11 11:42:31 +0200

    added initializer

--- initializer.py
+++ initializer.py
@@ -6,6 +6,13 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 w, h, r, n = None, None, None, None
 
+def sgn(x):
+    if x > 0.:
+        return 1.
+    elif x < 0.:
+        return -1.
+    return 0.
+
 def set_params(w_,h_,r_,n_):
     global w,h,r,n
     w = w_
@@ -30,20 +37,6 @@ def periodic_pos(sphere):
 
 ################################################################################
 
-def get_first_coll(spheres):
-    i0 = random.randint(0,n-1)
-    two_r = 2*r
-    for i in range(i0,n): #first loop
-        for j in range(n):
-            if i != j:
-                if d(spheres[i],spheres[j]) < two_r:
-                    return i
-    #
-    for i in range(0,i0): #second loop
-        for j in range(n):
-            if i != j:
-                if d(spheres[i],spheres[j]) < two_r:
-                    return i
 
 def count_collisions(spheres):
     counter = 0
@@ -55,25 +48,30 @@ def count_collisions(spheres):
                     counter += 1
     return counter
 
-def spring(spheres, vmax):
+
+def spring(spheres, factor, vmax):
     forces = [[0.,0.] for i in range(n)]
-    tolerance = 2*r #+0.05*r mais attention au counter
+    two_r = 2*r
+    tolerance = 0.1*r #mais attention au counter
     collisions = 0
     for i in range(n): #first loop
         for j in range(n):
             if i != j:
-                if d(spheres[i],spheres[j]) < tolerance:
+                distance = d(spheres[i],spheres[j])
+                if distance < two_r + tolerance:
+                    dq = distance - two_r
+                    intensity = -(dq-tolerance)
                     dx = spheres[j][0] - spheres[i][0]
                     dy = spheres[j][1] - spheres[i][1]
-                    forces[i][0] -= dx
-                    forces[i][1] -= dy
+                    forces[i][0] -= sgn(dx)*intensity
+                    forces[i][1] -= sgn(dy)*intensity
     for i in range(n):
-        vnorm = math.hypot(forces[i][0],forces[i][1])*SPRING
-        if vnorm > 0:
-            vx = vmax*forces[i][0]/vnorm
-            vy = vmax*forces[i][1]/vnorm
-            spheres[i][0] += vx
-            spheres[i][1] += vy
+        vnorm = math.hypot(forces[i][0],forces[i][1])*factor
+        if vnorm > vmax:
+            vx = forces[i][0]/vnorm
+            vy = forces[i][1]/vnorm
+            spheres[i][0] += vx*vmax
+            spheres[i][1] += vy*vmax
             periodic_pos(spheres[i])
             collisions += 1
     return collisions
@@ -96,34 +94,39 @@ def initialize_spheres(w,h,r,n,trials):
     draw(spheres, screen)
     collisions = count_collisions(spheres)
     collisions0 = float(collisions)
+    clock = pygame.time.Clock()
     for trial in range(trials):
-##        collisions = spring(spheres,collisions/collisions0)
-        collisions = spring(spheres,1.)
-        if trial%10 == 0:
+        clock.tick(45)
+##        collisions = spring(spheres,collisions/collisions0 + 0.2)
+        collisions = spring(spheres,1.,0.01)
+        if trial%1 == 0:
             draw(spheres, screen)
-            print("     ",trial,"collisions =",collisions)
+            if trial%100 == 0:
+                print("     ",trial,"collisions =",collisions)
         if collisions == 0:
             print("finished at", trial)
             break
     draw(spheres,screen)
+    kk = 0
     loop = True
     while loop:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 loop = False
-##            elif e.type == pygame.MOUSEMOTION:
-##                spheres[0] = e.pos
-##                draw(spheres, screen)
-##                print(d(spheres[0],spheres[1])<2*r, d(spheres[1],spheres[0])<2*r)
-##            elif e.type == pygame.MOUSEBUTTONDOWN:
-##                pass
+            elif e.type == pygame.MOUSEMOTION:
+                pass
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, (255,255,255),
+                                pygame.Rect(e.pos[0]-2,e.pos[1]-2,4,4))
+                pygame.display.flip()
+                kk += 1
+                print(kk)
 
     pygame.quit()
     return spheres
 
 random.seed(100000)
-SPRING = 1000.
-spheres = initialize_spheres(300, 300, 20, n=60, trials=int(1e3))
+spheres = initialize_spheres(300, 300, 20, n=60, trials=int(2e3))
 ################################################################################
 
 
